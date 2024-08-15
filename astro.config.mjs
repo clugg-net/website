4cd6506c54e1dae3e831d4cb83806159d3295ef2
Tyson <tyson@clugg.net>
2024-08-15 12:46:52 +0000

    Silence build errors/warnings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
