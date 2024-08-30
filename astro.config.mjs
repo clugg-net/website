03a9dd606a6ec0ce03c1eadd4d9a059d4cc2b0ae
Tyson <tyson@clugg.net>
2024-08-30 11:56:42 +0000

    Move to CMS based setup for all content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,84 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig, envField } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+// https://github.com/withastro/astro/issues/11820#issuecomment-2307610588
+process.env.ASTRO_TELEMETRY_DISABLED = "1";
 
 // import cloudflare from "@astrojs/cloudflare";
+const today = new Date();
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://clugg.net',
-  trailingSlash: 'always',
-  integrations: [mdx(), sitemap()],
-  "output": "static"
-  // output: "server",
-  // adapter: cloudflare()
+	experimental: {
+		env: {
+			schema: {
+				SITE_TITLE: envField.string({
+					context: "client",
+					access: "public",
+					optional: true,
+				}),
+				SITE_DESCRIPTION: envField.string({
+					context: "client",
+					access: "public",
+					optional: true,
+				}),
+				SITE_IMAGE: envField.string({
+					context: "client",
+					access: "public",
+					optional: true,
+				}),
+				SITE_URL: envField.string({
+					context: "client",
+					access: "public",
+					optional: true,
+				}),
+				COPYRIGHT_NOTICE: envField.string({
+					context: "client",
+					access: "public",
+					optional: true,
+				}),
+				COPYRIGHT_START_YEAR: envField.number({
+					context: "client",
+					access: "public",
+					optional: true,
+					min: 1710,
+					max: today.getFullYear(),
+				}),
+				COPYRIGHT_END_YEAR: envField.number({
+					context: "client",
+					access: "public",
+					default: today.getFullYear(),
+					min: 1710,
+					max: today.getFullYear(),
+				}),
+				COPYRIGHT_OWNER: envField.string({
+					context: "client",
+					access: "public",
+					optional: true,
+				}),
+			},
+		},
+		contentLayer: true,
+	},
+	site: process.env.SITE || "http://localhost/",
+	base: process.env.BASE || undefined,
+	trailingSlash: process.env.TRAILING_SLASH || "never",
+	integrations: [mdx(), sitemap()],
+	output: "static",
+	compressHTML: process.env.COMPRESS_HTML == true,
+	// https://vitejs.dev/config/build-options
+	vite: {
+		assetsInlineLimit: parseInt(
+			process.env.VITE_ASSETS_INLINE_LIMIT || "1024",
+			10,
+		),
+	},
+	build: {
+		format: process.env.BUILD_FORMAT || "preserve",
+		inlineStylesheets: process.env.BUILD_INLINE_STYLESHEETS || "auto",
+	},
+	prefetch: true,
+	// output: "server",
+	// adapter: cloudflare()
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,15 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+import { get_all_settings, type SettingsEntry } from "./lib/content/settings";
 
-export const SITE_TITLE = 'clugg.net';
-export const SITE_DESCRIPTION = 'Welcome to clugg.net';
+let settings: SettingsEntry | undefined = undefined;
+for (const entry of await get_all_settings()) {
+    if (entry.slug == "site") {
+        settings = entry;
+    }
+}
+
+export const SITE_TITLE = settings?.data?.title || process.env.SITE_TITLE || "SITE_TITLE";
+export const SITE_DESCRIPTION = settings?.body || process.env.SITE_DESCRIPTION || "SITE_DESCRIPTION";
+export const SITE_IMAGE = settings?.data?.image || process.env.SITE_IMAGE || "";
+export const SITE_URL = settings?.data?.url || process.env.SITE_URL || undefined;
